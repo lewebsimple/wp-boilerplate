f67622704f0db838bf5fcf96f5841a9fa1322bc1
Pascal Martineau <pascal@lewebsimple.ca>
2022-03-22 13:49:05 -0400

    Don't install WP so we can choose the language

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -13,8 +13,6 @@ mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
 # Download / configure / install WordPress
 wp core download --force --skip-content
 wp core config --dbname=${DB_NAME}
-wp language core install fr_CA
-wp core install --url=${PROJECT}.ledevsimple.ca --title=${PROJECT}
 
 # Create plugins / themes / uploads directories
 mkdir -p wp-content/plugins wp-content/themes wp-content/uploads
@@ -23,3 +21,6 @@ mkdir -p wp-content/plugins wp-content/themes wp-content/uploads
 git init
 git add -A
 git commit -am 'Initial WordPress project'
+
+# Done
+echo "Site ready at http://${PROJECT}.${TLD-ledevsimple.ca}"

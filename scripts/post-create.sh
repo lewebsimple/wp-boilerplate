333366a13cff9a0070649a7a46aebf755fad5ecf
Pascal Martineau <pascal@lewebsimple.ca>
2023-08-17 09:43:20 -0400

    fix :git stuff

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -9,32 +9,29 @@ GIT_ORIGIN="ssh://git@gitlab.ledevsimple.ca:222/wp-sites/${PROJECT}.git"
 # Rename project
 sed -i -e "s/wp-boilerplate/${PROJECT}/g" .cpanel.yml .gitignore composer.json
 
-# Create database
-mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-
-# Download / configure / install WordPress
-wp core download --force --skip-content
-wp core config --dbname=${DB_NAME}
-
-# Create plugins / themes / uploads directories
-mkdir -p wp-content/mu-plugins wp-content/plugins wp-content/themes wp-content/uploads
-
 # Initialize git repository
 git init
 git remote add gitlab ${GIT_ORIGIN}
-git fetch || error_code=$?
+git fetch gitlab || error_code=$?
 if [ "${error_code-0}" -eq 0 ]; then
   git checkout main -f
   rm -rf wp-content/mu-plugins/*
   rm -rf wp-content/plugins/*
   rm -rf wp-content/themes/*
-  git reset --hard HEAD
+  git reset --hard
   composer install
 else
   git remote remove gitlab
   git add -A
   git commit -am 'chore: initial wp-boilerplate project'
 fi
+
+# Create database
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+
+# Download / configure / install WordPress
+wp core download --force --skip-content
+wp core config --dbname=${DB_NAME}
 
 # Done
 echo "Site ready at http://${PROJECT}.${TLD-ledevsimple.ca}"
